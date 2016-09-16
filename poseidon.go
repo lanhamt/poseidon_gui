e3b473e9b703c088005830f551da2d52c5df9c24
lanhamt <tlanham@cs.stanford.edu>
2016-09-16 09:36:20 -0700

    added decision engine pic

--- poseidon.go
+++ poseidon.go
@@ -22,6 +22,13 @@ const (
 <h1 style="color:chartreuse"> <big><big><big> Welcome to Poseidon </big></big></big> </h1>
 <body style="background-color:black;">
 
+<!-- decision engine -->
+<p>
+<a href="http://localhost:15672">
+<img src="http://sciencenordic.com/sites/default/files/imagecache/620x/rabbit_0.jpg" alt="decision engine" width="800" height="600" border="0">
+</a>
+</p>
+
 <!-- rabbit -->
 <p>
 <a href="http://localhost:15672">
